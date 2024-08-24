5b568f3dd2113e18e2f9d26379544bd599bdcc17
dr1602 <david.arangelg@gmail.com>
2024-08-23 19:18:00 -0600

    First Commit

---
+++ 1_print.py
@@ -0,0 +1,16 @@
+# Gato o hashtag son para comentar, este codigo no se correra
+
+# funcion print para string o texto
+print('Este es un mensaje')
+print("Este es otro mensaje")
+
+# Tambien se puede imprimir operaciones:
+
+print(2 + 2)
+print(2 / 3)
+
+# Tambien se puede imprimir tipos de datos 
+print(type('Esto es un mensaje'))
+print(type(2+2))
+
+# para correr el comando uso control + alt + n, pero depende de la configuracion de cada sistema
---
+++ 2_clase3.py
@@ -0,0 +1,7 @@
+saludo_1 = "Hola"
+saludo_2 = "Hi"
+nombre_1 = "Carli"
+nombre_2 = "карли"
+
+print(saludo_1)
+print(nombre_2)
