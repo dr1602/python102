114294adc1d61c9e111221ed4d20529f2b601445
dr1602 <david.arangelg@gmail.com>
2024-08-24 21:10:59 -0600

    types 24ago24

---
+++ 4_types.py
@@ -0,0 +1,25 @@
+# entero
+x = 10
+
+# flotantes
+y = 3.141592365
+z = 1.2e12
+a = 1.2e-6
+
+print(type(x))
+print(type(y))
+print(z)
+print(type(z))
+print(a)
+print(type(a))
+
+print(x + x)
+print(x + y)
+print(y + y)
+
+# booleano, la primer letra tiene que ser mayuscula
+is_true = True
+is_false = False
+
+print(type(is_true))
+print(is_false)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,5 @@
-first_name = 'CARLA Marcela'
-last_name = '   Florida y RUMANA   '
+is_true = True
+is_false = False
 
-print(first_name)
-print(last_name)
-print(first_name + ' ' + last_name)
-
-print (first_name * 6)
-print (3 * last_name)
-
-# consultas
-
-print(len(last_name))
-print(len(first_name))
-
-# metodos para los strings, porquees propia del string como hay metodos para arrays
-
-print(first_name.lower())
-print(last_name.upper())
-print(last_name.strip())
+print(type(is_true))
+print(is_false)
