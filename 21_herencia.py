f3b32f60a02de05603313aa05f2a1446f9e3529a
dr1602 <david.arangelg@gmail.com>
2024-08-26 10:29:59 -0600

    herencia2

--- 21_herencia.py
+++ 21_herencia.py
@@ -26,15 +26,80 @@ class Vehicle:
         # levantamos excepcion
         raise NotImplementedError('Este metodo debe ser implementado por la subclase')
     
-    class Car(Vehicle):
-        def start_engine(self):
-            if not self.is_available:
-                return f'El motor del coche {self.model} esta en marcha'
-            else:
-                return f'El  coche {self.brand} no esta disponible'
+class Car(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f'El motor del coche {self.model} esta en marcha'
+        else:
+            return f'El  coche {self.brand} no esta disponible'
             
-        def stop_engine(self):
-            if not self.is_available:
-                return f'El motor del coche {self.model} se ha apagado.'
-            else:
-                return f'El  coche {self.brand} no esta disponible'
+    def stop_engine(self):
+        if not self.is_available:
+            return f'El motor del coche {self.model} se ha apagado.'
+        else:
+            return f'El  coche {self.brand} no esta disponible'
+            
+class Bike(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f'La bicicleta {self.model} esta en marcha'
+        else:
+            return f'La bicicleta {self.brand} no esta disponible'
+            
+    def stop_engine(self):
+        if not self.is_available:
+            return f'La bicicleta {self.model} se ha detenido.'
+        else:
+            return f'La bicicleta {self.brand} no esta disponible'
+        
+class Truck(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f'El motor del camion {self.model} esta en marcha'
+        else:
+            return f'El  camion {self.brand} no esta disponible'
+            
+    def stop_engine(self):
+        if not self.is_available:
+            return f'El motor del camion {self.model} se ha apagado.'
+        else:
+            return f'El  camion {self.brand} no esta disponible'
+        
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.purchased_vehicles = []
+        
+    def buy_car(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            vehicle.sell()
+            self.purchased_vehicles.append(vehicle)
+        else:
+            print(f'Lo sient, {vehicle.brand} no esta disponible')
+            
+    def inquire_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            availability = 'Disponible'
+        else:
+            availability = 'No disponible'
+        print(f'El estatus del {vehicle.brand} es {availability} y cuesta {vehicle.get_price()}')
+        
+class Dealership:
+    def __init__(self):
+        self.inventory = []
+        self.customers = []
+        
+    def add_vehicles(self, vehicle: Vehicle):
+        self.inventory.append(vehicle)
+        print(f'{vehicle.brand} ha sido agregado al inventario.')
+        
+    def add_customer(self, customer: Customer):
+        self.customers.append(customer)
+        print(f'La o el cliente {customer.name} ha sido agregado a la lista de clientes.')
+        
+    def show_available_vehicles(self):
+        print('Los vehiculos disponibles en la tienda son: ')
+        for vehicle in inventory:
+            if vehicle.check_available():
+                print(f'- {vehicle.brand} por {vehicle.get_price()}')
+    
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,106 +1,105 @@
-class Car:
-    def __init__(self, name, model, price):
-        self.name = str(name)
-        self.model = int(model)
-        self.price = int(price)
-        self.business_owned = True
-        self.person_owned = False
-
-    def sold_by_business(self, budget):
-        if self.business_owned:
-            if self.price <= budget:
-                self.business_owned = False
-                self.person_owned = True
-                print(f'El carro {self.name} fue vendido por el negocio')
-            else:
-                print(f'El carro {self.name} no puede ser vendido a la persona por falta de presupuesto')
+class Vehicle:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.is_available = True
+        
+    def sell(self):
+        if self.is_available:
+            self.is_available = False
+            print(f'El vehiculo {self.brand}, ha sido vendido.')
         else:
-            print(f'El carro {self.name} no puede ser vendido ya que no se encuentra disponible en el negocio.')
+            print(f'El vehiculo {self.brand}, no esta disponible.')
+            
+    def check_available(self):
+        return self.is_available
+    
+    def get_price(self):
+        return self.price
 
-    def sold_by_person(self, budget):
-        if self.person_owned:
-            if self.price <= budget:
-                self.person_owned = False
-                self.business_owned = True
-                print(f'El carro {self.name} fue vendido por la persona')
-            else:
-                print(f'El carro {self.name} no puede ser comprado por el negocio por falta de presupuesto')
+    def start_engine(self):
+        # levantamos excepcion
+        raise NotImplementedError('Este metodo debe ser implementado por la subclase')
+
+    def stop_engine(self):
+        # levantamos excepcion
+        raise NotImplementedError('Este metodo debe ser implementado por la subclase')
+    
+class Car(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f'El motor del coche {self.model} esta en marcha'
         else:
-            print(f'El carro {self.name} no puede ser vendido ya que la persona no cuenta con el carro.')
-
-class Person:
-    def __init__(self, name, budget):
-        self.name = str(name)
-        self.budget = int(budget)
-        self.owned_cars = []
-
-    def buy_car(self, car):
-        if car.business_owned:
-            if car.price <= self.budget:
-                car.sold_by_business(self.budget)  # Pasa el presupuesto de la persona
-                self.owned_cars.append(car)
-                self.budget -= car.price
-                print(f'Usted ha comprado el carro {car.name}, por {car.price}. Ahora su presupuesto disponible es: {self.budget}')
-            else:
-                print(f'Usted no puede comprar el carro {car.name}, le faltan {car.price - self.budget}.')
+            return f'El  coche {self.brand} no esta disponible'
+            
+    def stop_engine(self):
+        if not self.is_available:
+            return f'El motor del coche {self.model} se ha apagado.'
         else:
-            print(f'El carro {car.name} no está a la venta en el negocio')
-
-    def sell_car(self, car, business):
-        if car.person_owned:
-            if car.price <= business.budget:
-                car.sold_by_person(business.budget)  # Pasa el presupuesto del negocio
-                self.owned_cars.remove(car)
-                self.budget += car.price
-                business.budget -= car.price
-                print(f'Usted ha vendido el carro {car.name}, por {car.price}. Ahora su presupuesto disponible es: {self.budget}')
-            else:
-                print(f'El negocio no tiene suficiente dinero para comprar el carro {car.name}.')
+            return f'El  coche {self.brand} no esta disponible'
+            
+class Bike(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f'La bicicleta {self.model} esta en marcha'
         else:
-            print(f'Usted no tiene el carro {car.name} por lo que no lo puede vender')
-
-class Business:
-    def __init__(self, name, budget):
-        self.name = str(name)
-        self.budget = int(budget)
-        self.owned_cars = []
-
-    def buy_car(self, car, person):
-        if car.person_owned:
-            if car.price <= self.budget:
-                car.sold_by_person(self.budget)  # Pasa el presupuesto del negocio
-                self.owned_cars.append(car)
-                self.budget -= car.price
-                person.budget += car.price
-                print(f'El negocio ha comprado el carro {car.name}, por {car.price}. Ahora su presupuesto disponible es: {self.budget}')
-            else:
-                print(f'El negocio no puede comprar el carro {car.name}, le faltan {car.price - self.budget}.')
+            return f'La bicicleta {self.brand} no esta disponible'
+            
+    def stop_engine(self):
+        if not self.is_available:
+            return f'La bicicleta {self.model} se ha detenido.'
         else:
-            print(f'El carro {car.name} no está a la venta por la persona')
-
-    def sell_car(self, car, person):
-        if car.business_owned:
-            if car.price <= person.budget:
-                car.sold_by_business(person.budget)  # Pasa el presupuesto de la persona
-                self.owned_cars.remove(car)
-                self.budget += car.price
-                person.budget -= car.price
-                print(f'El negocio ha vendido el carro {car.name}, por {car.price}. Ahora su presupuesto disponible es: {self.budget}')
-            else:
-                print(f'La persona no tiene suficiente dinero para comprar el carro {car.name}.')
+            return f'La bicicleta {self.brand} no esta disponible'
+        
+class Truck(Vehicle):
+    def start_engine(self):
+        if not self.is_available:
+            return f'El motor del camion {self.model} esta en marcha'
         else:
-            print(f'El negocio no tiene el carro {car.name} por lo que no lo puede vender')
-
-# Crear autos
-car1 = Car('Toyota', 2020, 10000)
-car2 = Car('Honda', 2021, 12000)
-
-# Crear persona y negocio
-person1 = Person('Carlos', 15000)
-business = Business('AutoDealer', 50000)
-
-# El negocio vende el carro a la persona
-person1.buy_car(car1)
-
-# La persona vende el carro de vuelta al negocio
-person1.sell_car(car1, business)
+            return f'El  camion {self.brand} no esta disponible'
+            
+    def stop_engine(self):
+        if not self.is_available:
+            return f'El motor del camion {self.model} se ha apagado.'
+        else:
+            return f'El  camion {self.brand} no esta disponible'
+        
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.purchased_vehicles = []
+        
+    def buy_car(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            vehicle.sell()
+            self.purchased_vehicles.append(vehicle)
+        else:
+            print(f'Lo sient, {vehicle.brand} no esta disponible')
+            
+    def inquire_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            availability = 'Disponible'
+        else:
+            availability = 'No disponible'
+        print(f'El estatus del {vehicle.brand} es {availability} y cuesta {vehicle.get_price()}')
+        
+class Dealership:
+    def __init__(self):
+        self.inventory = []
+        self.customers = []
+        
+    def add_vehicles(self, vehicle: Vehicle):
+        self.inventory.append(vehicle)
+        print(f'{vehicle.brand} ha sido agregado al inventario.')
+        
+    def add_customer(self, customer: Customer):
+        self.customers.append(customer)
+        print(f'La o el cliente {customer.name} ha sido agregado a la lista de clientes.')
+        
+    def show_available_vehicles(self):
+        print('Los vehiculos disponibles en la tienda son: ')
+        for vehicle in inventory:
+            if vehicle.check_available():
+                print(f'- {vehicle.brand} por {vehicle.get_price()}')
+    
