8f5b2ff22f6fe2a4b860bc8ce207465c29151616
dr1602 <david.arangelg@gmail.com>
2024-08-25 20:35:53 -0600

    lambda

--- 15_lambda_funcional.py
+++ 15_lambda_funcional.py
@@ -1,3 +1,11 @@
+add = lambda a, b: a + b
+print(add(10,4))
+
+multiply = lambda a, b: a * b
+print(multiply(80,5))
+
+# cuadrado de cada numero
+
 numbers = range(11)
 squared_numbers = list(map(lambda x: x ** 2, numbers))
 print('Cuadrados:', squared_numbers)
@@ -8,4 +16,4 @@ print('Pares:', even_numbers)
 
 # obtener los numeros pares
 odd_numbers = list(filter(lambda x: x%2 != 0, numbers))
-print('Pares:', odd_numbers)
+print('Impares:', odd_numbers)
