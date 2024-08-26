b6c69d759893852919f667a080b4debe1cb4317d
dr1602 <david.arangelg@gmail.com>
2024-08-25 21:37:37 -0600

    recursividad corregida

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,9 @@
-def sumatoria_numeros_naturales(n):
-    return (( n * (n + 1) )/2)
+def sumatoria(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return n + sumatoria(n - 1)
 
-sumatoria = print(sumatoria_numeros_naturales(100))
+sumatoria = print(sumatoria(6))
